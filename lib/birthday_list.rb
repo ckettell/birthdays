eebf1defcc12a5db34cca63c498c0c222d5f0998
Charles Kettell <charleskettell@hotmail.co.uk>
2019-06-27 11:30:32 +0100

    first commit

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -1,1 +1,11 @@
 
+class Friends
+attr_accessor :entries
+
+  def initialize
+    @entries = []
+  end
+  def value_adder(name, age)
+    @entries << {:name => name, :age => age}
+  end
+end
--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -1,1 +1,22 @@
+require 'birthday_list'
 
+RSpec.describe Friends do
+  describe '#initialize' do
+    it 'initialises with zero entries' do
+      friend = Friends.new
+      expect(friend.entries). to eq []
+    end
+  end
+end
+
+# describe Friends do
+#   it 'should add name and age hash' do
+#     expect(subject.value_adder('Charlie', 24)). to eq true {:name => 'Charlie', :age => 24}
+#   end
+# end
+#
+# RSpec.describe BirthdayList do
+#   it "should be true" do
+#     expect(subject.dog). to eq true
+#   end
+# end
